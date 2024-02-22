5a36e73a4381f9a5872857a1098c4d60ef878f10
annibal <annibalvillegas17@gmail.com>
2024-02-22 07:45:56 -0600

    m

--- lib/Functions.py
+++ lib/Functions.py
@@ -12,5 +12,27 @@ def LVR(modo, inOrderArr):
         inOrderArr.append(modoPadre.valor)
         LVR(modoPadre.Derecha,inOrderArr)
     return inOrderArr
+def VLR(modo, PreOrderArr):
+    if modo is not None:
+        modoPadre = modo
+        PreOrderArr.append(modoPadre.valor)
+        VLR(modoPadre.izquierda, PreOrderArr)
+        VLR(modoPadre.Derecha,PreOrderArr)
+    return PreOrderArr
+def LRV(modo, PostOrderArr):
+    if modo is not None:
+        modoPadre = modo
+        LRV(modoPadre.izquierda, PostOrderArr)
+        LRV(modoPadre.Derecha,PostOrderArr)
+        PostOrderArr.append(modoPadre.valor)
+    return PostOrderArr
+def modoOrdenados(modoPadre, newModo):
+    if newModo.valor < modoPadre.valor:
+        if modoPadre.izquierda is None:
+            modoPadre.izquierda = newModo
+    if newModo.valor > modoPadre.valor:
+        if modoPadre.Derecha is None:
+            modoPadre.Derecha = newModo
+    pass
 
 
--- lib/classes.py
+++ lib/classes.py
@@ -8,11 +8,13 @@ class modo():
         return f'Valor:  {self.valor}'
     
     def getArbol(self):
-        strOut = ""
-        strOut += f" NP [{self.valor}]"
+        strOut = ''
+        strOut += f'NP: [{self.valor}] '
         if type(self.izquierda) != type(None):
-            strOut += f"[{self.valor}]->[{self.izquierda}]"
-        elif self.Derecha is not None:
-            strOut += f"[{self.valor}]->[{self.Derecha}]"
-            return strOut
-pass        
+            strOut += f'Iz: [{self.valor}] -> [{self.izquierda}]'
+        if self.Derecha is not None:
+            strOut += f'Dr: [{self.valor}] -> [{self.Derecha}]'
+        return strOut
+    def __str__(self):
+        return f'{self.valor}'
+        
